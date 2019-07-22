333a613fd9dec4f2964630252a8b141cc8aa6a52
Gia Khang <levogiakhang@gmail.com>
2019-07-22 16:33:41 +0700

    Add fake data

---
+++ src/Demo/utils/FakeData.js
@@ -0,0 +1,7 @@
+export const fakeData = {
+  itemId: 'id_',
+  userName: 'user_',
+  msgContent: "",
+  avatar: "https://randomuser.me/api/portraits/thumb/men/",
+  timestamp: "2009-01-25T09:51:56Z",
+};
