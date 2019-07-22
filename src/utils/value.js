a90b5a9b67a8e35344e161cc18bce5abeca7247f
Gia Khang <levogiakhang@gmail.com>
2019-07-22 15:30:56 +0700

    Add value and type

---
+++ src/utils/value.js
@@ -0,0 +1,6 @@
+export const NOT_FOUND = -1;
+export const NOT_UNIQUE = -2;
+export const OUT_OF_RANGE = 'out of range';
+export const THROTTLING_TIMER = 200;
+export const DEBOUNCING_TIMER = 200;
+export const DEFAULT_HEIGHT = 150;
