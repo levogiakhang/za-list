4b576d113cd0bda51584432dbff5af5b5f8870dd
Gia Khang <levogiakhang@gmail.com>
2019-07-22 15:36:01 +0700

    Add CellMeasurer

---
+++ src/CellMeasurer/CellMeasurer.js
@@ -0,0 +1,68 @@
+// @flow
+
+import * as React from 'react';
+import * as ReactDOM from "react-dom";
+import ResizeObserver from 'resize-observer-polyfill';
+import throttle from "../vendors/throttle";
+import { THROTTLING_TIMER } from "../utils/value";
+import { Position } from "../utils/types";
+import CellMeasurerViewModel from "../ViewModel/CellMeasurerViewModel";
+
+type OnChangedHeightCallback = any;
+
+type CellMeasurerProps = {
+  id: string,
+  defaultHeight: number,
+  position?: Position,
+  isVirtualized: boolean,
+  onChangedHeight: OnChangedHeightCallback,
+}
+
+export default class CellMeasurer extends React.PureComponent<CellMeasurerProps> {
+  constructor(props) {
+    super(props);
+    this._cellMeasurer = undefined;
+    this.resizeObserver = undefined;
+    this.viewModel = undefined;
+  }
+
+  componentDidMount() {
+    this._cellMeasurer = ReactDOM.findDOMNode(this);
+    this.viewModel = new CellMeasurerViewModel(
+      {
+        node: this._cellMeasurer,
+        props: this.props
+      });
+    this.resizeObserver = new ResizeObserver(throttle(this.viewModel.onChildrenChangeHeight, THROTTLING_TIMER));
+    this.resizeObserver.observe(this._cellMeasurer);
+  }
+
+  componentWillUnmount() {
+    this.resizeObserver.disconnect(this._cellMeasurer);
+  }
+
+  render() {
+    const {
+      id,
+      className,
+      children,
+      position: {top, left},
+      isVirtualized
+    } = this.props;
+
+    return (
+      <div id={id}
+           className={className}
+           style={{
+             display: 'block',
+             position: isVirtualized ? 'absolute' : 'relative',
+             top: top,
+             left: left,
+             overflow: 'auto',
+             width: '100%'
+           }}>
+        {children}
+      </div>
+    );
+  }
+}
---
+++ src/ViewModel/CellMeasurerViewModel.js
@@ -0,0 +1,36 @@
+class CellMeasurerViewModel {
+  constructor({node: cellMeasurer, props}) {
+    this.cellMeasurer = cellMeasurer;
+    this.props = props;
+
+    this._oldHeight = this.props.defaultHeight;
+    this._newHeight = undefined;
+    this.onChildrenChangeHeight = this.onChildrenChangeHeight.bind(this);
+  }
+
+  onChildrenChangeHeight() {
+    if (this._isChangedHeight()) {
+      const oldHeightCache = this._oldHeight;
+      this._oldHeight = this._newHeight;
+      this.props.onChangedHeight(this.props.id, oldHeightCache, this._newHeight);
+    }
+  };
+
+  _isChangedHeight() {
+    this._newHeight = this._getCellHeight();
+    return this._oldHeight !== this._newHeight;
+  }
+
+  _getCellHeight() {
+    if (
+      this.cellMeasurer &&
+      this.cellMeasurer.ownerDocument &&
+      this.cellMeasurer.ownerDocument.defaultView &&
+      this.cellMeasurer instanceof this.cellMeasurer.ownerDocument.defaultView.HTMLElement
+    ) {
+      return Math.round(this.cellMeasurer.offsetHeight);
+    }
+  }
+}
+
+export default CellMeasurerViewModel;
