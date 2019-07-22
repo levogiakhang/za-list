adc89c36ed54126dbb4b52978c07fe28f4b94b22
Gia Khang <levogiakhang@gmail.com>
2019-07-22 17:18:19 +0700

    Add ItemCache

---
+++ src/utils/ItemCache.js
@@ -0,0 +1,106 @@
+import { NOT_FOUND } from "./value";
+
+class ItemCache {
+  constructor() {
+    // A map stores `index -> itemId`
+    this.__indexMap__ = new Map();
+
+    // A map stores `itemId -> {index, defaultHeight, height, position, isDebut}`
+    this.__itemsMap__ = new Map();
+  }
+
+  clear() {
+    this.__indexMap__.clear();
+    this.__itemsMap__.clear();
+  }
+
+  getItemId(index: number): string {
+    return this.__indexMap__.has(index) ? this.__indexMap__.get(index) : NOT_FOUND;
+  }
+
+  hasItem(itemId: string): boolean {
+    return this.__itemsMap__.has(itemId);
+  }
+
+  getIndex(itemId: string): number {
+    return this.__itemsMap__.has(itemId) ? this.__itemsMap__.get(itemId).index : NOT_FOUND
+  }
+
+  getDefaultHeight(itemId: string): number {
+    return this.__itemsMap__.has(itemId) ? this.__itemsMap__.get(itemId).defaultHeight : NOT_FOUND
+  }
+
+  getHeight(itemId: string): number {
+    return this.__itemsMap__.has(itemId) ? this.__itemsMap__.get(itemId).height : NOT_FOUND
+  }
+
+  getPosition(itemId: string): number {
+    return this.__itemsMap__.has(itemId) ? this.__itemsMap__.get(itemId).position : NOT_FOUND
+  }
+
+  isRendered(itemId: string): boolean {
+    return this.__itemsMap__.has(itemId) ? this.__itemsMap__.get(itemId).isRendered : NOT_FOUND
+  }
+
+  get getIndexMap() {
+    return this.__indexMap__;
+  }
+
+  get getItemsMap() {
+    return this.__itemsMap__;
+  }
+
+  deleteItem(itemIndex, itemId, data) {
+    // Update index map
+    this.updateIndexMap(itemIndex - 1, data);
+    this.__indexMap__.delete(data.length);
+
+    // Update items map.
+    this.updateItemsMap(itemIndex - 1, data);
+    this.__itemsMap__.delete(itemId);
+  }
+
+  updateItemOnMap(itemId: string, itemIndex: number, defaultHeight: number, itemHeight: number, itemPosition: number, isRendered: boolean) {
+    this.__itemsMap__.set(
+      itemId,
+      {
+        index: itemIndex,
+        defaultHeight: defaultHeight,
+        height: itemHeight,
+        position: itemPosition,
+        isDebut: isRendered
+      });
+  }
+
+  updateIndexItem(index: number, itemId: string) {
+    this.__indexMap__.set(index, itemId);
+  }
+
+  updateItemsMap(startIndex: number, data: Array) {
+    if (!!data.length) {
+      let itemId;
+      if (startIndex < 0) startIndex = 0;
+      for (let i = startIndex; i <= data.length - 1; i++) {
+        itemId = this.getItemId(i);
+        this.updateItemOnMap(
+          itemId,
+          i,
+          this.getDefaultHeight(itemId),
+          this.getHeight(itemId),
+          this.getPosition(itemId),
+          this.isRendered(itemId));
+      }
+    }
+  }
+
+  updateIndexMap(startIndex: number, data: Array) {
+    if (!!data.length) {
+      if (startIndex < 0) startIndex = 0;
+      for (let i = startIndex; i < data.length; i++) {
+        this.updateIndexItem(i, data[i].itemId);
+      }
+    }
+  }
+}
+
+export default ItemCache;
