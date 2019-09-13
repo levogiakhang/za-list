9e584352514f469c0df3d7ff45fdede98a5af9e9
Gia Khang <levogiakhang@gmail.com>
2019-09-13 17:23:58 +0700

    :bug: Fix HOC not recognize when default height equals real height

--- src/ViewModel/CellMeasurerViewModel.js
+++ src/ViewModel/CellMeasurerViewModel.js
@@ -3,13 +3,14 @@ class CellMeasurerViewModel {
     this.cellMeasurer = cellMeasurer;
     this.props = props;
 
+    this.isFirst = true;
     this._oldHeight = this.props.defaultHeight;
     this._newHeight = undefined;
     this.onChildrenChangeHeight = this.onChildrenChangeHeight.bind(this);
   }
 
   onChildrenChangeHeight() {
-    if (this._isChangedHeight()) {
+    if (this._isChangedHeight() || this.isFirst) {
       const oldHeightCache = this._oldHeight;
       this._oldHeight = this._newHeight;
       this.props.onChangedHeight(this.props.id, oldHeightCache, this._newHeight);
@@ -17,6 +18,9 @@ class CellMeasurerViewModel {
   };
 
   _isChangedHeight() {
+    if (this._newHeight !== undefined) {
+      this.isFirst = false;
+    }
     this._newHeight = this._getCellHeight();
     return this._oldHeight !== this._newHeight;
   }
