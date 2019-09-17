b9afb36ae77ee970cf3cd44017a5f302be570ce0
Gia Khang <levogiakhang@gmail.com>
2019-09-17 18:11:28 +0700

    Add toggle class

--- src/View/AnimationExecution.js
+++ src/View/AnimationExecution.js
@@ -1,6 +1,7 @@
 export const AnimExecution = {
   appendStyle,
   removeStyle,
+  toggleClass
 };
 
 function appendStyle(el, animationNames) {
@@ -98,5 +99,19 @@ function removeClass(el, className) {
         el.setAttribute('class', replaceClassName(el.className && el.className.baseVal || '', className));
       }
     }
+  }
+}
+
+function toggleClass(el, className) {
+  if (
+    el &&
+    el.classList &&
+    typeof el.classList.toggle === "function"
+  ) {
+    el.classList.toggle(className);
+  } else if (hasClass(el, className)) {
+    removeClass(el, className);
+  } else {
+    addClass(el, className);
   }
 }
